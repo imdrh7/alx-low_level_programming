9a588fcd5a4121836ef5a393928d68aaf95db14a
imdrh7 <mahmoudrabih00@gmail.com>
2023-09-27 06:39:45 -0700

    add code

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,22 @@
-#include <stdio.h>
-#include "listint.h" // Include the custom header file
+#include "lists.h"
 
-int main() {
-    // Example usage:
-    listint_t node1, node2, node3;
-    node1.n = 1;
-    node1.next = &node2;
-    node2.n = 2;
-    node2.next = &node3;
-    node3.n = 3;
-    node3.next = NULL;
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
 
-    size_t node_count = print_listint(&node1);
-    printf("Number of nodes: %zu\n", node_count);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
 
-    return 0;
+	return (num);
 }
+
