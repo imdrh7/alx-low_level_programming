3f139c7140580ab491fdc7451bc2b91d8d3168af
imdrh7 <mahmoudrabih00@gmail.com>
2023-10-28 12:10:08 -0700

    l

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>		
-#include "main.h"
 		
 
 		
@@ -26,9 +25,6 @@ int main(int argc, char *argv[])
 (void) argv; /*Ignore argv*/
 		
 	printf("%d\n", argc - 1);
-		
-
-		
 	return (0);
 		
 }
