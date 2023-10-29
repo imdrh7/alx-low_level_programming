9d991cf6159176ff1f9b5aebfc1e64e96f9fc5fa
imdrh7 <mahmoudrabih00@gmail.com>
2023-10-28 23:47:59 -0700

    ll

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,15 +8,17 @@
  * @argv:pointer to an array of commandline arguments.
  * Return:0-success, non-zero-fail.
  */
-				
-int main(int argc, char *argv[] __attribute__((unused)))
-		
-{
-		
-(void) argv; 
-		
-	printf("%d\n", argc - 1);
-	return (0);
-		
-}
 
+int main(int argc, char *argv[])
+
+{
+
+(void) argv;
+
+	printf("%d\n", argc - 1);
+
+
+
+	return (0);
+
+}
