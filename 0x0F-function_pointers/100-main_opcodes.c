9c54424fddafc016fa909bf46b940ea3f7fa6b42
imdrh7 <mahmoudrabih00@gmail.com>
2023-11-08 17:30:57 -0800

    kk

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,48 +1,26 @@
+#include "function_pointers.h"
+#include <stdlib.h>
 #include <stdio.h>
-	#include <stdlib.h>
 
+/**
+ * main - check the code for the schoole student.
+ * @argc: the number of args
+ * @argv: argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
 
-	/**
-	 * main - prints its own opcodes
-	 * @argc: number of arguments
-	 * @argv: array of arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int main(int argc, char *argv[])
-	{
-		int bytes, i;
-		char *arr;
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
 
-
-		if (argc != 2)
-		{
-			printf("Error\n");
-			exit(1);
-		}
-
-
-		bytes = atoi(argv[1]);
-
-
-		if (bytes < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-
-		arr = (char *)main;
-
-
-		for (i = 0; i < bytes; i++)
-		{
-			if (i == bytes - 1)
-			{
-				printf("%02hhx\n", arr[i]);
-				break;
-			}
-			printf("%02hhx ", arr[i]);
-		}
-		return (0);
-	}
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
